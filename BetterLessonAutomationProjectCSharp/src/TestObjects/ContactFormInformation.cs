03e6979ee3f4dc1e78418f44d488548466feded5
n-fowler <45500369+n-fowler@users.noreply.github.com>
2022-04-01 05:29:58 -0400

    small cleanup

--- BetterLessonAutomationProjectCSharp/src/TestObjects/ContactFormInformation.cs
+++ BetterLessonAutomationProjectCSharp/src/TestObjects/ContactFormInformation.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BetterLessonAutomationProjectCSharp.src.TestObjects
+﻿namespace BetterLessonAutomationProjectCSharp.src.TestObjects
 {
     public class ContactFormInformation
     {
